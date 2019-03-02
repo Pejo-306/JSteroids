59038bd69bbc47d94eaccdbeb00e896624c70676
Petar Nikolov <penikolov23@gmail.com>
2019-03-02 12:40:33 +0200

    [alpha] Set several physics properties for Player. Document player code.
    The player's game object now wraps around the game world. Damping has
    been turned on for the player.
    
    Additionally, the current player class has been documented more
    thoroughly with JSDoc style comments.

--- scripts/objects/Player.js
+++ scripts/objects/Player.js
@@ -1,28 +1,88 @@
+/**
+ * @author       Petar Nikolov <penikolov23@gmail.com>
+ * @copyright    2019 Petar Nikolov
+ * @license      {@link https://github.com/Pejo-306/JSteroids/blob/master/LICENSE|MIT License}
+ */
+
 import 'phaser';
 
 import GameObject from './GameObject.js';
 
 /**
+ * @classdesc
  * Player controlled game object.
  *
- * Contains all the logic required to spawn and control the player's 
+ * Contains all game logic required to spawn and control the player's 
  * game object. The latter is represented by a spaceship which flies around
  * in space and may shoot objects.
+ *
+ * @class Player
+ * @since 0.1.0
  */
 class Player extends GameObject {
 
+    /**
+     * Starting orientation.
+     *
+     * @static
+     * @method Player.STARTING_ANGLE
+     * @since 0.1.0
+     *
+     * @return {number} The starting angle.
+     */
     static get STARTING_ANGLE () { return -90; }
 
-    static get DRAG () { return 50; }
+    /**
+     * Game object's drag.
+     *
+     * @static
+     * @method Player.DRAG
+     * @since 0.1.0
+     *
+     * @return {number} Drag value.
+     */
+    static get DRAG () { return 0.99; }
 
+    /**
+     * Max velocity of player's physics sprite.
+     *
+     * @static
+     * @method Player.MAX_VELOCITY
+     * @since 0.1.0
+     *
+     * @return {number} Max velocity value.
+     */
     static get MAX_VELOCITY () { return 200; }
 
+    /**
+     * Angular velocity which is set when rotating the player's sprite.
+     *
+     * @static
+     * @method Player.ANGULAR_VELOCITY
+     * @since 0.1.0
+     *
+     * @return {number} Angular velocity value.
+     */
     static get ANGULAR_VELOCITY () { return 200; }
+    
+    /**
+     * Padding added to each world boundary edge.
+     *
+     * @static
+     * @method Player.WRAP_PADDING
+     * @since 0.1.0
+     *
+     * @return {number} Wrap padding value.
+     */
+    static get WRAP_PADDING () { return 32; }
 
     /**
      * Construct player's game object.
      *
-     * @param {Game} game Reference to the Phaser game instance
+     * @constructor
+     * @since 0.1.0
+     *
+     * @param {Game} game - Reference to the Phaser game instance.
      */
     constructor (game) {
         super(game);
@@ -33,7 +93,11 @@ class Player extends GameObject {
     /**
      * Preload resources required by player's game object.
      *
-     * @param {Phaser.Scene} scene The current game scene
+     * @static
+     * @method Player.preload
+     * @since 0.1.0
+     *
+     * @param {Phaser.Scene} scene - The current game scene.
      */
     static preload (scene) {
         scene.load.image('player', 'assets/sprites/player.png');
@@ -42,17 +106,25 @@ class Player extends GameObject {
     /**
      * Spawn the Player in the game world.
      *
-     * This method creates the game object's physics sprite.
+     * This method creates the game object's physics sprite and sets all
+     * appropriate physics related properties.
      *
-     * @param {number} x X coordinates of spawn position
-     * @param {number} y Y coordinates of spawn position
+     * @method Player#spawn
+     * @since 0.1.0
+     *
+     * @param {number} x - X coordinates of spawn position.
+     * @param {number} y - Y coordinates of spawn position.
+     *
+     * @return {Player} This player instance.
      */
     spawn (x, y) {
         this.sprite = this.scene.physics.add.sprite(x, y, 'player');
         this.sprite.body.setAllowGravity(false);
-        this.sprite.setDrag(this.constructor.DRAG);
+        this.sprite.setDamping(true);
+        this.sprite.setDrag(this.constructor.DRAG, this.constructor.DRAG);
         this.sprite.body.setMaxVelocity(this.constructor.MAX_VELOCITY);
         this.sprite.angle = this.constructor.STARTING_ANGLE;
+        return this;
     }
 
     /**
@@ -60,6 +132,9 @@ class Player extends GameObject {
      *
      * The player may control his/her object via the cursor keys - accelerate 
      * forwards, as well as rotate in both directions.
+     *
+     * @method Player#update
+     * @since 0.1.0
      */
     update () {
         let cursors = this.scene.input.keyboard.createCursorKeys();
@@ -81,6 +156,9 @@ class Player extends GameObject {
         } else {
             this.sprite.setAngularVelocity(0);
         }
+
+        // Keep the player within the physics world bounds.
+        this.scene.physics.world.wrap(this.sprite, this.constructor.WRAP_PADDING);
     }
 
 }
